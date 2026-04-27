268b1a4011ae449b9574d49d0415b72b1aff6799
Andrew Abramajtys <anab639@student.oaisd.org>
2026-04-27 14:43:54 -0400

    stuff

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float hp;
+    public float speed;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    public float atk;
+    public float def;
+    public float jumpForce;
+    public float checkRadius = 0.2f;
+    public Transform groundCheck;
+    public bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +25,5 @@ public class Player : MonoBehaviour
     {
         
     }
+    
 }
