fc27cba542188709c119c67ab56ea875b63f685b
Andrew Abramajtys <anab639@student.oaisd.org>
2026-04-28 14:43:57 -0400

    addded jumping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,29 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Player : MonoBehaviour
 {
     public float hp;
     public float speed;
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
     Vector2 movement;
     public float atk;
     public float def;
     public float jumpForce;
     public float checkRadius = 0.2f;
     public Transform groundCheck;
-    public bool isGrounded;
-    // Start is called before the first frame update
+    public LayerMask groundLayer;
+    public bool isGrounded = false;
+    private float horizontalInput;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
     }
-    
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
+    }
 }
