d54e94a88b21ebd1434070373d444f82559ee8ff
Andrew Abramajtys <anab639@student.oaisd.org>
2026-04-29 14:38:20 -0400

    Made map, added double jump, added sprint

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float hp;
-    public float speed;
+    public float speed = 5f;
     private Rigidbody2D rb;
     Vector2 movement;
     public float atk;
@@ -14,22 +14,44 @@ public class Player : MonoBehaviour
     public Transform groundCheck;
     public LayerMask groundLayer;
     public bool isGrounded = false;
-    private float horizontalInput;
+    [SerializeField]private int jumpsRemaining = 2;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetKey(KeyCode.LeftShift))
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            speed = 10f;
         }
-    }
-    void FixedUpdate()
-    {
-        rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
+        else
+        {
+            speed = 5f;
+        }
+        float moveX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float moveZ = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.Translate(moveX, 0, moveZ);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
+        if (isGrounded)
+        {
+            jumpsRemaining = 2;
+        }
+        if (Input.GetButtonDown("Jump") && jumpsRemaining >= 1)
+        {
+            if (!isGrounded)
+            {
+                jumpsRemaining--;
+            }
+            if (jumpsRemaining <= 1)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce * 1.5f);
+            }
+            else
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            }
+            jumpsRemaining--;
+        }
     }
 }
